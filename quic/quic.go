c8f42f9ea9e69a3fb4ba4aab77ef6643b968ba30
mehrdad arshad rad <mehrdad@edgecast.com>
2019-10-23 22:51:40 -0700

    added more connection control

--- quic/quic.go
+++ quic/quic.go
@@ -1,82 +1,111 @@
 package quic
 
-import(
+import (
+	"context"
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/tls"
 	"crypto/x509"
 	"encoding/pem"
-	"math/big"
-	"context"
-	"log"
 	"io"
+	"log"
+	"math/big"
+	"time"
 
 	quicgo "github.com/lucas-clemente/quic-go"
 	"github.com/songgao/water"
 )
 
+const buffMaxSize = 1300
 
 type QUIC struct {
-	TUNIf	   *water.Interface
+	TUNIf      *water.Interface
 	RemoteHost string
 }
 
 func (q QUIC) Run() {
 
 	// from remote to tun interface
-	go func(){
+	go func() {
 		config := &quicgo.Config{
-			KeepAlive: true,
+			IdleTimeout: 2 * time.Second,
+			KeepAlive:   true,
 		}
-		listener, err := quicgo.ListenAddr(":8085", generateTLSConfig(), config)
-		if err != nil {
-			log.Fatal(err)
-		}
+		for {
+			listener, err := quicgo.ListenAddr(":8085", generateTLSConfig(), config)
+			if err != nil {
+				log.Println("listen",err)
+				continue
+			}
 
-		session, err := listener.Accept(context.Background())
-		if err != nil {
-			log.Fatal(err)
-		}
+			session, err := listener.Accept(context.Background())
+			if err != nil {
+				log.Fatal(err)
+			}
 
-		stream, err := session.AcceptStream(context.Background())
-		if err != nil {
-			log.Fatal(err)
-		}
+			stream, err := session.AcceptStream(context.Background())
+			if err != nil {
+				log.Fatal(err)
+			}
 
-		io.Copy(q.TUNIf, stream)
+			io.Copy(q.TUNIf, stream)
+
+			stream.Close()
+			session.Close()
+			listener.Close()
+
+			log.Println("DONE from remote to tun")
+		}
 	}()
 
 	// from tun interface to remote
-	go func(){
-		buff := make([]byte, 1024)
+	go func() {
+		buff := make([]byte, buffMaxSize)
 		tlsConf := &tls.Config{
 			InsecureSkipVerify: true,
 			NextProtos:         []string{"radvpn"},
 		}
-
-		session, err := quicgo.DialAddr(q.RemoteHost, tlsConf, nil)
-		if err != nil {
-			log.Fatal(err)
+		config := &quicgo.Config{
+			HandshakeTimeout : 2 * time.Second,
+			IdleTimeout: 2 * time.Second,
+			KeepAlive:   true,
 		}
 
-		stream, err := session.OpenStreamSync(context.Background())
-		if err != nil {
-			log.Fatal(err)	
-		}
-
-		for{ 
-			n, err := q.TUNIf.Read(buff)
+		for {
+			log.Println("start from tun to if")
+		
+			session, err := quicgo.DialAddr(q.RemoteHost, tlsConf, config)
 			if err != nil {
-				continue	
+				log.Println("dial", err)
+				continue
 			}
 
-			stream.Write([]byte(buff[:n]))
-		}	
+			stream, err := session.OpenStreamSync(context.Background())
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			for {
+				n, err := q.TUNIf.Read(buff)
+				if err != nil {
+					continue
+				}
+
+				_, err = stream.Write([]byte(buff[:n]))
+				if err != nil {
+					log.Println("stream", err)
+					break
+				}
+			}
+
+			stream.Close()
+			session.Close()
+		}
 	}()
 }
 
 func generateTLSConfig() *tls.Config {
-	key, err := rsa.GenerateKey(rand.Reader, 1024)
+	key, err := rsa.GenerateKey(rand.Reader, buffMaxSize)
 	if err != nil {
 		panic(err)
 	}
@@ -87,7 +116,7 @@ func generateTLSConfig() *tls.Config {
 	}
 
 	keyPEM := pem.EncodeToMemory(&pem.Block{
-		Type: "RSA PRIVATE KEY", 
+		Type:  "RSA PRIVATE KEY",
 		Bytes: x509.MarshalPKCS1PrivateKey(key),
 	})
 
@@ -99,8 +128,8 @@ func generateTLSConfig() *tls.Config {
 	}
 
 	return &tls.Config{
-		Certificates: []tls.Certificate{tlsCert},
-		NextProtos:   []string{"radvpn"},
+		Certificates:       []tls.Certificate{tlsCert},
+		NextProtos:         []string{"radvpn"},
 		ClientSessionCache: nil,
 	}
 }
