6fd90d898af8f2fd76784c4859cdf71077f8914f
mehrdad rad <arshad.rad@gmail.com>
2019-11-01 07:16:05 -0700

    code refactored

--- main.go
+++ main.go
@@ -1,25 +1,54 @@
 package main
 
 import (
-	"context"
 	"flag"
 	"net"
 	"time"
 	"log"
+	"context"
 
-	"github.com/mehrdadrad/radvpn/crypto"
-	"github.com/mehrdadrad/radvpn/netdev"
+	// "github.com/mehrdadrad/radvpn/crypto"
+	"github.com/mehrdadrad/radvpn/server"
 	"github.com/mehrdadrad/radvpn/router"
-	"github.com/mehrdadrad/radvpn/udp"
+	// "github.com/mehrdadrad/radvpn/udp"
 )
-
-type server interface {
-	Start(context.Context)
-}
 
 var localHost = flag.String("local", "10.0.2.1/24", "IP/Mask")
 var remoteHost = flag.String("remote", "192.168.55.10:8085", "IP:Port")
 
+func main() {
+	flag.Parse()
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	server.SetupTunInterface([]string{*localHost}, 1300)
+
+
+	r := router.New()
+
+	_, dst, _ := net.ParseCIDR("10.0.1.0/24")
+	nexthop := net.ParseIP("192.168.55.10")
+	err := r.Table().Add(dst, nexthop)
+	if err != nil {
+		log.Println(err)
+	}
+
+	_, dst, _ = net.ParseCIDR("10.0.2.0/24")
+	nexthop = net.ParseIP("192.168.55.20")
+	err = r.Table().Add(dst, nexthop)
+	if err != nil {
+		log.Println(err)
+	}
+
+	s := server.Server{
+		KeepAlive: 10 * time.Second,
+		Router: r,
+	}
+
+	s.Run(ctx, 10, 10)
+}
+
+
+/*
 func main() {
 	var srv server
 
@@ -32,6 +61,8 @@ func main() {
 	}
 
 	ifce := netdev.New([]string{*localHost}, 1300)
+	//ifce1 := netdev.New([]string{*localHost}, 1300)
+	//_ = ifce1
 	r := router.New()
 
 	_, dst, _ := net.ParseCIDR("10.0.1.0/24")
@@ -53,7 +84,7 @@ func main() {
 	srv = &udp.UDP{
 		TunIfce:     ifce,
 		RemoteHosts: []string{*remoteHost},
-		MaxThreads:  100,
+		MaxThreads:  10,
 		KeepAlive:   10 * time.Second,
 		Cipher:      crp,
 		Router:      r,
@@ -64,3 +95,4 @@ func main() {
 
 	select {}
 }
+*/
---
+++ server/server.go
@@ -0,0 +1,290 @@
+package server
+
+import (
+	"context"
+	"errors"
+	"log"
+	"net"
+	"sync"
+	"syscall"
+	"time"
+
+	"github.com/mehrdadrad/radvpn/router"
+
+	"golang.org/x/sys/unix"
+
+	"github.com/songgao/water"
+	"github.com/vishvananda/netlink"
+)
+
+const maxBufsize = 1518
+
+// Server represents vpn server
+type Server struct {
+	KeepAlive     time.Duration
+	Router        router.Gateway
+
+	maxWorkers int
+
+	read  chan []byte
+	write chan []byte
+}
+
+type tun struct {
+	maxWorkers int
+	ifces chan *water.Interface
+	ii *water.Interface
+
+	read  chan []byte
+	write chan []byte
+}
+
+type header struct {
+	version int
+	src     net.IP
+	dst     net.IP
+}
+
+// Run stars workers
+func (s Server) Run(ctx context.Context, maxTunWorkers, maxNetWorkers int) {
+	bp := &sync.Pool{
+		New: func() interface{} {
+			return make([]byte, maxBufsize)
+		},
+	}
+
+	s.Router.Table().Dump()
+
+	s.maxWorkers = maxNetWorkers
+
+	s.read = make(chan []byte, 1000)
+	s.write = make(chan []byte, 1000)
+
+	t := new(tun)
+	t.maxWorkers = maxTunWorkers
+
+	t.read = make(chan []byte, 1000)
+	t.write = make(chan []byte, 1000)
+
+	go t.run(ctx, bp)
+	go s.run(ctx, bp)
+
+	s.cross(t)
+
+	<-ctx.Done()
+}
+
+func (s Server) run(ctx context.Context, bp *sync.Pool) {
+	for i := 0; i < s.maxWorkers; i++ {
+		conn, err := s.listenPacket(ctx)
+		if err != nil {
+			log.Fatal(err)
+
+		}
+
+		go s.reader(ctx, conn, bp)
+		go s.writer(ctx, conn, bp)
+	}
+}
+
+func (s *Server) cross(t *tun) {
+	go func() {
+		for {
+			b := <-s.read
+			t.write <- b
+		}
+	}()
+
+	go func() {
+		for {
+			b := <-t.read
+			s.write <- b
+		}
+	}()
+}
+
+func (s Server) listenPacket(ctx context.Context) (net.PacketConn, error) {
+	lc := net.ListenConfig{
+		Control: func(network, address string, c syscall.RawConn) error {
+			var sockoptErr error
+			err := c.Control(func(fd uintptr) {
+				sockoptErr = unix.SetsockoptInt(
+					int(fd),
+					unix.SOL_SOCKET,
+					unix.SO_REUSEPORT,
+					1,
+				)
+			})
+
+			if err != nil {
+				return err
+			}
+			return sockoptErr
+		},
+		KeepAlive: s.KeepAlive,
+	}
+
+	return lc.ListenPacket(ctx, "udp", ":8085")
+}
+
+func (s *Server) reader(ctx context.Context, conn net.PacketConn, bp *sync.Pool) {
+
+	for {
+		b := bp.Get().([]byte)
+		n, _, err := conn.ReadFrom(b)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+
+		select {
+		case s.read <- b[:n]:
+		default:
+		}
+	}
+}
+
+func (s *Server) writer(ctx context.Context, conn net.PacketConn, bp *sync.Pool) {
+	for {
+		b := <-s.write
+
+		h, err := parseHeader(b)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+
+		nexthop := s.Router.Table().Get(h.dst)
+		rAddr, _ := net.ResolveUDPAddr("udp", nexthop.String()+":8085")
+
+		_, err = conn.WriteTo(b, rAddr)
+		if err != nil {
+			log.Println(err)
+		}
+	}
+}
+
+// run stars workers to read/write from tunnel
+func (t tun) run(ctx context.Context, bp *sync.Pool) {
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	for i := 0; i < t.maxWorkers; i++ {
+		ifce, err := createTunInterface()
+		if err != nil {
+			log.Fatal(err)
+		}
+		go t.reader(ctx, ifce, bp, i)
+		go t.writer(ctx, ifce, bp, i)
+	}
+
+	<-ctx.Done()
+}
+
+// reader reads from tun interface
+func (t *tun) reader(ctx context.Context, ifce *water.Interface, bp *sync.Pool, i int) {
+	//ifce := <- t.ifces
+
+	for {
+		log.Println("threaad read #", i)
+		b := bp.Get().([]byte)
+		n, err := ifce.Read(b)
+		if err != nil {
+			log.Println(err)
+		}
+
+		select {
+		case t.read <- b[:n]:
+		default:
+		}
+	}
+}
+
+// writer writes to tun interface
+func (t *tun) writer(ctx context.Context, ifce *water.Interface, bp *sync.Pool, i int) {
+	//ifce := <- t.ifces
+
+	for {
+		log.Println("threaad write #", i)
+		b := <-t.write
+		_, err := ifce.Write(b)
+		if err != nil {
+			log.Println(err)
+		}
+	}
+}
+
+// SetupTunInterface creates and sets tun interface attributes
+func SetupTunInterface(ipaddrs []string, mtu int) error {
+
+	ifname := "radvpn"
+	config := water.Config{
+		DeviceType: water.TUN,
+		PlatformSpecificParams: water.PlatformSpecificParams{
+			Name:       ifname,
+			MultiQueue: true,
+		},
+	}
+
+	_, err := water.New(config)
+	if err != nil {
+		return err
+	}
+
+	ifce, _ := netlink.LinkByName(ifname)
+	netlink.LinkSetMTU(ifce, mtu)
+	netlink.LinkSetUp(ifce)
+
+	for _, ipnet := range ipaddrs {
+		addr, err := netlink.ParseAddr(ipnet)
+		if err != nil {
+			return err
+		}
+		err = netlink.AddrAdd(ifce, addr)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// createTunInterface creates a cloned tun interface
+func createTunInterface() (*water.Interface, error) {
+	ifname := "radvpn"
+	config := water.Config{
+		DeviceType: water.TUN,
+		PlatformSpecificParams: water.PlatformSpecificParams{
+			Name:       ifname,
+			MultiQueue: true,
+		},
+	}
+
+	return water.New(config)
+}
+
+func parseHeader(b []byte) (*header, error) {
+	if len(b) < net.IPv4len {
+		return nil, errors.New("small packet")
+	}
+
+	h := new(header)
+
+	h.version = int(b[0] >> 4)
+
+	if h.version == 4 {
+		h.src = make(net.IP, net.IPv4len)
+		copy(h.src, b[12:16])
+		h.dst = make(net.IP, net.IPv4len)
+		copy(h.dst, b[16:20])
+
+		return h, nil
+	}
+
+	h.src = make(net.IP, net.IPv6len)
+	copy(h.src, b[8:24])
+	h.dst = make(net.IP, net.IPv6len)
+	copy(h.dst, b[24:40])
+
+	return h, nil
+}
